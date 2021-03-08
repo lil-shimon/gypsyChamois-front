fbd5c91a39b63f2f9493626521a68febbbd16f8d
shimosawa <shimosawa@questar.ac>
2021-03-08 22:12:15 +0900

    create contact page

--- components/Layout..tsx
+++ components/Layout..tsx
@@ -35,7 +35,7 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
             </main>
             <footer className='w-full h-12 flex justify-center items-center border-t'>
               <a
-                  className='flex items-center'
+                  className='flex items-centerj'
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
