13c5e844a3ef7e63a9ad5e843f25bd570bfae33e
shimosawa <shimosawa@questar.ac>
2021-03-09 20:57:38 +0900

    show page

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,13 +1,16 @@
 import {FC} from "react";
+import Link from "next/link";
 
 const Post: FC<{ post: any }> = ({post}) => {
     return (
         <div>
             <span>{post.id}</span>
             {" : "}
+            <Link href={`/posts/${post.id}`}>
             <span className='cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200'>
                 {post.title}
             </span>
+            </Link>
         </div>
     )
 }
--- lib/posts.ts
+++ lib/posts.ts
@@ -2,8 +2,34 @@ import fetch from 'node-fetch'
 
 const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
+//一覧
 export async function getAllPropsData() {
     const res = await fetch(new URL(apiUrl))
     const posts = await res.json()
     return posts
+}
+
+//IDの一覧
+//getStaticPathsの場合はparamsを指定
+export async function getAllPostIds() {
+    const res = await fetch(new URL(apiUrl));
+    const posts = await res.json();
+
+    return posts.map((post: any) => {
+        return {
+            params: {
+                id: String(post.id),
+            },
+        };
+    });
+}
+
+// show
+export async function getPostData(id: number) {
+    const res = await fetch(new URL(`${apiUrl}/${id}`))
+    const post = await res.json()
+
+    return {
+        post
+    }
 }
