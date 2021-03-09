b55cf97cf1d59e037dd851f65eb1947fe789f90a
shimosawa <shimosawa@questar.ac>
2021-03-09 21:09:51 +0900

    edit

--- pages/contact-page.tsx
+++ pages/contact-page.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/Layout.";
 //imageの圧縮をしてくれる
 import Image from "next/image";
 
-const Contact = () => {
+export default function Contact(){
     return (
         <Layout title={Contact}>
             <div className='bg-white text-center shadow-xl p-8 w-80 rounded'>
@@ -75,5 +75,3 @@ const Contact = () => {
         </Layout>
     )
 }
-
-export default Contact
